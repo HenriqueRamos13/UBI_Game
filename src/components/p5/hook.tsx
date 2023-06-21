300b5810f20c0b86f051a52f1affa49a6579f54b
HenriqueRamos13 <henrique.mrcr@gmail.com>
2023-06-22 00:18:23 +0100

    only move user

--- src/components/p5/hook.tsx
+++ src/components/p5/hook.tsx
@@ -1,85 +1,39 @@
+"use client";
+
 import { useState, useEffect } from "react";
 import p5Types from "p5";
+import supabaseClient from "../../utils/supabaseClient";
+import { RealtimeChannel } from "@supabase/supabase-js";
 
 const BLOCK_SIZE = 32;
 
-const USED_KEYS = {
-  ArrowUp: "ArrowUp",
-  ArrowDown: "ArrowDown",
-  ArrowLeft: "ArrowLeft",
-  ArrowRight: "ArrowRight",
-  w: "w",
-  s: "s",
-  a: "a",
-  d: "d",
-  space: " ",
-  q: "q",
-  e: "e",
-};
-
-interface Arrow {
+interface Axis {
   axis: "x" | "y";
   value: number;
 }
 
-const arrows: { [key: string]: Arrow } = {
-  ArrowUp: {
-    axis: "y",
-    value: -BLOCK_SIZE,
-  },
-  ArrowDown: {
-    axis: "y",
-    value: BLOCK_SIZE,
-  },
-  ArrowLeft: {
-    axis: "x",
-    value: -BLOCK_SIZE,
-  },
-  ArrowRight: {
-    axis: "x",
-    value: BLOCK_SIZE,
-  },
-};
-
 interface WASD {
-  [key: string]: Arrow;
+  [key: string]: Axis;
 }
 
 const wasd: WASD = {
-  w: {
-    axis: "y",
-    value: -BLOCK_SIZE,
-  },
   s: {
     axis: "y",
+    value: -BLOCK_SIZE,
+  },
+  x: {
+    axis: "y",
     value: BLOCK_SIZE,
   },
-  a: {
+  z: {
     axis: "x",
     value: -BLOCK_SIZE,
   },
-  d: {
+  c: {
     axis: "x",
     value: BLOCK_SIZE,
   },
 };
-
-const powersKeys: { [key: string]: string } = {
-  space: "space",
-  q: "q",
-  e: "e",
-};
-
-interface Power {
-  id: string;
-  x: number;
-  y: number;
-  color: string;
-  maxDistance: number;
-  distanceTravelled: number;
-  lastMovementTime: number;
-  movementPeriod: number;
-}
 
 interface Player {
   id: string;
@@ -87,136 +41,157 @@ interface Player {
   y: number;
   color: string;
   maxRange: number;
-}
-
-interface LastPlayerMovement {
-  axis: "x" | "y";
-  value: number;
-}
-
-interface Cursor {
-  x: number;
-  y: number;
-  color: string;
+  presence_ref?: string | null;
 }
 
 const useP5 = () => {
   const [roomId, setRoomId] = useState("");
-  const [players, setPlayers] = useState<Player[]>([
-    {
-      id: "",
-      x: 128,
-      y: 128,
-      color: "yellow",
-      maxRange: 4,
-    },
-  ]);
-  const [powers, setPowers] = useState<Power[]>([]);
-  const [power, setPower] = useState<Power | null>(null);
+  const [enemy, setEnemy] = useState<Player | null>(null);
   const [player, setPlayer] = useState<Player>({
-    id: "",
+    id: "user" + Math.floor(Math.random() * 90000) + 10000,
     x: 64,
     y: 64,
     color: "blue",
     maxRange: 4,
+    presence_ref: null,
   });
-  const [lastArrowMovement, setLastArrowMovement] =
-    useState<LastPlayerMovement>({
-      axis: "x",
-      value: BLOCK_SIZE,
-    });
-  const [cursor, setCursor] = useState<Cursor | null>(null);
+
+  // Channel name can be any string.
+  // Create channels with the same name for both the broadcasting and receiving clients.
+  const [channel, setChannel] = useState<RealtimeChannel>(
+    supabaseClient.channel("room-6")
+  );
 
   useEffect(() => {
-    console.log("lastArrowMovement", lastArrowMovement);
-  }, [lastArrowMovement]);
-
-  function verifyIfPowerHitAPlayer(power: Power) {
-    return players.some((player) => {
-      return (
-        player.x === power.x &&
-        player.y === power.y &&
-        player.color !== power.color
-      );
+    const channelUsers = supabaseClient.channel("online-users-room-6", {
+      config: {
+        presence: {
+          key: player.id,
+        },
+      },
     });
-  }
+
+    channelUsers.on("presence", { event: "sync" }, () => {
+      console.log("Online users: ", channelUsers.presenceState());
+
+      if (
+        !player.presence_ref &&
+        player.id &&
+        channelUsers.presenceState()[player.id]
+      ) {
+        setPlayer((prevPlayer) => {
+          return {
+            ...prevPlayer,
+            presence_ref: (
+              channelUsers.presenceState()[prevPlayer.id] as any
+            )[0].presence_ref,
+          };
+        });
+      }
+
+      console.log("xxx", channelUsers.presenceState());
+
+      Object.keys(channelUsers.presenceState()).forEach((key) => {
+        console.log("tttttt", player, key);
+        if (key === player.id) return;
+
+        console.log("yyyyy", channelUsers.presenceState()[key]);
+
+        setEnemy((prevEnemy) => ({
+          id: key,
+          x: 64,
+          y: 64,
+          color: "yellow",
+          maxRange: 4,
+          presence_ref: channelUsers.presenceState()[key][0].presence_ref,
+        }));
+      });
+    });
+
+    channelUsers.on("presence", { event: "join" }, ({ newPresences }) => {
+      console.log("New users have joined: ", newPresences);
+    });
+
+    channelUsers.on("presence", { event: "leave" }, ({ leftPresences }) => {
+      console.log("Users have left: ", leftPresences);
+      if (leftPresences[0].presence_ref === enemy?.presence_ref) {
+        setEnemy(null);
+      }
+    });
+
+    channelUsers.subscribe(async (status) => {
+      if (status === "SUBSCRIBED") {
+        const status = await channelUsers.track({
+          online_at: new Date().toISOString(),
+        });
+        console.log(status);
+      }
+    });
+
+    const channelLatency = supabaseClient.channel("calc-latency", {
+      config: {
+        broadcast: { ack: true },
+      },
+    });
+
+    channelLatency.subscribe(async (status) => {
+      if (status === "SUBSCRIBED") {
+        const begin = performance.now();
+
+        await channelLatency.send({
+          type: "broadcast",
+          event: "latency",
+          payload: {},
+        });
+
+        const end = performance.now();
+
+        console.log(`Latency is ${end - begin} milliseconds`);
+      }
+    });
+
+    channel
+      .subscribe((status) => {
+        console.log("channel status", status);
+        if (status === "SUBSCRIBED") {
+          // now you can start broadcasting cursor positions
+          console.log("channel subscribed");
+        }
+      })
+      .on("broadcast", { event: "player_move" }, (payload) => {
+        console.log("player moved payload", payload);
+        setEnemy((prevEnemy) => {
+          if (!prevEnemy) {
+            return null;
+          }
+
+          if (prevEnemy?.id === payload.payload.id) {
+            return {
+              ...prevEnemy,
+              x: payload.payload.x,
+              y: payload.payload.y,
+            };
+          }
+          return prevEnemy;
+        });
+      });
+
+    return () => {
+      channelUsers.unsubscribe();
+      channelLatency.unsubscribe();
+      channel.unsubscribe();
+    };
+  }, []);
 
   function draw(p5: p5Types) {
     p5.background(100);
 
-    // for each player draw a blue square
-    players.forEach((player) => {
-      p5.fill(player.color);
-      p5.rect(player.x, player.y, BLOCK_SIZE, BLOCK_SIZE);
-    });
-
     p5.fill(player.color);
     p5.rect(player.x, player.y, BLOCK_SIZE, BLOCK_SIZE);
 
-    if (cursor) {
-      p5.fill(cursor.color);
-      p5.rect(cursor.x, cursor.y, BLOCK_SIZE, BLOCK_SIZE);
-    }
-
-    if (power) {
-      if (verifyIfPowerHitAPlayer(power)) {
-        setPower(null);
-        return;
-      }
-
-      if (power.distanceTravelled >= power.maxDistance) {
-        setPower(null);
-        return;
-      }
-
-      p5.fill(power.color);
-
-      if (p5.millis() - power.lastMovementTime < power.movementPeriod) {
-        p5.rect(power.x, power.y, BLOCK_SIZE, BLOCK_SIZE);
-      } else {
-        switch (lastArrowMovement.axis) {
-          case "x":
-            if (lastArrowMovement.value > 0) {
-              p5.rect(power.x + BLOCK_SIZE, power.y, BLOCK_SIZE, BLOCK_SIZE);
-              setPower({
-                ...power,
-                x: power.x + BLOCK_SIZE,
-                distanceTravelled: (power.distanceTravelled += BLOCK_SIZE),
-                lastMovementTime: p5.millis(),
-              });
-            }
-            if (lastArrowMovement.value < 0) {
-              p5.rect(power.x - BLOCK_SIZE, power.y, BLOCK_SIZE, BLOCK_SIZE);
-              setPower({
-                ...power,
-                x: power.x - BLOCK_SIZE,
-                distanceTravelled: (power.distanceTravelled += BLOCK_SIZE),
-                lastMovementTime: p5.millis(),
-              });
-            }
-            break;
-          case "y":
-            if (lastArrowMovement.value > 0) {
-              p5.rect(power.x, power.y + BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-              setPower({
-                ...power,
-                y: power.y + BLOCK_SIZE,
-                distanceTravelled: (power.distanceTravelled += BLOCK_SIZE),
-                lastMovementTime: p5.millis(),
-              });
-            }
-            if (lastArrowMovement.value < 0) {
-              p5.rect(power.x, power.y - BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-              setPower({
-                ...power,
-                y: power.y - BLOCK_SIZE,
-                distanceTravelled: (power.distanceTravelled += BLOCK_SIZE),
-                lastMovementTime: p5.millis(),
-              });
-            }
-            break;
-        }
-      }
+    if (enemy) {
+      p5.fill(enemy.color);
+      p5.rect(enemy.x, enemy.y, BLOCK_SIZE, BLOCK_SIZE);
     }
   }
 
@@ -232,141 +207,24 @@ const useP5 = () => {
     if ((key as string) in wasd) {
       const { axis, value } = wasd[key as string];
 
-      setPlayer((player) => ({
+      const newPlayerPosition = {
         ...player,
         [axis]: player[axis] + value,
+      };
+
+      setPlayer((player) => ({
+        ...newPlayerPosition,
       }));
 
-      if (cursor) {
-        let moveCursor = false;
-
-        if (key === USED_KEYS.w) {
-          if (cursor.y - player.y > player.maxRange * BLOCK_SIZE) {
-            moveCursor = true;
-          }
-        }
-        if (key === USED_KEYS.s) {
-          if (player.y - cursor.y > player.maxRange * BLOCK_SIZE) {
-            moveCursor = true;
-          }
-        }
-        if (key === USED_KEYS.a) {
-          if (cursor.x - player.x > player.maxRange * BLOCK_SIZE) {
-            moveCursor = true;
-          }
-        }
-        if (key === USED_KEYS.d) {
-          if (player.x - cursor.x > player.maxRange * BLOCK_SIZE) {
-            moveCursor = true;
-          }
-        }
-
-        if (moveCursor) {
-          setCursor(
-            (cursor) =>
-              ({
-                ...cursor,
-                [axis]: cursor![axis] + value,
-              } as Cursor)
-          );
-        }
-      }
-    }
-
-    if ((key as string) in arrows) {
-      const { axis, value } = arrows[key as string];
-
-      if (cursor) {
-        let moveCursor = true;
-        if (key === USED_KEYS.ArrowUp) {
-          // verify if the cursor is "maxRange" away from the player on the positive y axis. If it is, don't move the cursor
-          if (player.y - cursor.y > player.maxRange * BLOCK_SIZE) {
-            moveCursor = false;
-          }
-        }
-        if (key === USED_KEYS.ArrowDown) {
-          // verify if the cursor is "maxRange" away from the player on the negative y axis. If it is, don't move the cursor
-          if (cursor.y - player.y > player.maxRange * BLOCK_SIZE) {
-            moveCursor = false;
-          }
-        }
-        if (key === USED_KEYS.ArrowLeft) {
-          // verify if the cursor is "maxRange" away from the player on the negative x axis. If it is, don't move the cursor
-          if (player.x - cursor.x > player.maxRange * BLOCK_SIZE) {
-            moveCursor = false;
-          }
-        }
-        if (key === USED_KEYS.ArrowRight) {
-          // verify if the cursor is "maxRange" away from the player on the positive x axis. If it is, don't move the cursor
-          if (cursor.x - player.x > player.maxRange * BLOCK_SIZE) {
-            moveCursor = false;
-          }
-        }
-
-        if (moveCursor) {
-          setCursor(
-            (cursor) =>
-              ({
-                ...cursor,
-                [axis]: cursor![axis] + value,
-              } as Cursor)
-          );
-        }
-      }
-
-      setPower((power) => {
-        if (power) {
-          return {
-            ...power,
-            [axis]: power[axis] + value,
-          };
-        } else {
-          return null;
-        }
+      channel!.send({
+        type: "broadcast",
+        event: "player_move",
+        payload: {
+          x: newPlayerPosition.x,
+          y: newPlayerPosition.y,
+          id: player.id,
+        },
       });
-
-      setLastArrowMovement({
-        axis,
-        value,
-      });
-    }
-
-    if ((key as any) in powersKeys) {
-      if (key === USED_KEYS.e) {
-        if (!power) {
-          setCursor(null);
-
-          const newPower = {
-            id: Math.random().toString(),
-            x: player.x,
-            y: player.y,
-            color: "red",
-            maxDistance: BLOCK_SIZE * 10,
-            distanceTravelled: 0,
-            lastMovementTime: p5.millis(),
-            movementPeriod: 100,
-          };
-
-          setPower(newPower);
-        }
-      }
-
-      if (key === USED_KEYS.q) {
-        // put the cursor close to the player based on the last arrow movement
-        if (lastArrowMovement.axis === "x") {
-          setCursor({
-            x: player.x + lastArrowMovement.value,
-            y: player.y,
-            color: "green",
-          });
-        } else {
-          setCursor({
-            x: player.x,
-            y: player.y + lastArrowMovement.value,
-            color: "green",
-          });
-        }
-      }
     }
   }
 
